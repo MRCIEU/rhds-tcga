0156a4e55c1f53c6e6b42f16f034439852a684a0
yousefi138 <paul.yousefi@bristol.ac.uk>
2025-01-31 17:44:21 +0000

    paths general and relative to script dir

--- extract-data.r
+++ extract-data.r
@@ -15,14 +15,17 @@ extract.file <- function(tar.file, extract.file, new.file){
 	unlink(dirname(x.file), recursive = TRUE)
 }
 
+data.dir <- paths$data.dir 
 
 #######################
 ## extract the clinical data
-clinical.file <- "clinical.txt"
+clinical.file <- file.path(data.dir, "clinical.txt")
 if(!file.exists(clinical.file))
 	extract.file( 
-		tar.file = grep(".*_HNSC\\..*_Clinical\\.Level_1\\..*\\.tar\\.gz$", 
-					list.files(), value = T),
+		tar.file = 
+			file.path(data.dir,
+				grep(".*_HNSC\\..*_Clinical\\.Level_1\\..*\\.tar\\.gz$", 
+					list.files(data.dir), value = T)),
 		extract.file = "HNSC.clin.merged.txt",
 		new.file = clinical.file
 		)
@@ -30,27 +33,31 @@ if(!file.exists(clinical.file))
 
 ########################
 ## extract the protein data
-protein.file <- "protein.txt"
+protein.file <- file.path(data.dir, "protein.txt")
 if(!file.exists(protein.file))
 	extract.file( 
-		tar.file = grep("*_protein_normalization__data.Level_3.*.tar.gz", 
-					list.files(), value = T),
+		tar.file = 
+			file.path(data.dir,
+				grep("*_protein_normalization__data.Level_3.*.tar.gz$", 
+					list.files(data.dir), value = T)),
 		extract.file = "data.txt",
 		new.file = protein.file
 		)
 ## clean protein output:
 ## 	- remove 2nd row
 lines <- readLines(protein.file)[-2]
-writeLines(lines, "protein-clean.txt")
+writeLines(lines, file.path(data.dir, "protein-clean.txt"))
 
 
 ########################
 ## extract the methylation data
-methylation.file <- "methylation.txt"
+methylation.file <- file.path(data.dir, "methylation.txt")
 if(!file.exists(methylation.file))
 	extract.file( 
-		tar.file = grep(".*_HNSC\\..*_humanmethylation450_.*_data\\.Level_3\\..*\\.tar\\.gz$", 
-					list.files(), value = T),
+		tar.file = 
+			file.path(data.dir,
+				grep(".*_HNSC\\..*_humanmethylation450_.*_data\\.Level_3\\..*\\.tar\\.gz$", 
+					list.files(data.dir), value = T)),
 		extract.file = "data.txt",
 		new.file = methylation.file
 		)
@@ -64,6 +71,20 @@ awk_command <- "
 	print \"\"
 	}' methylation.txt | sed 2d  > methylation-clean.txt
 	"
+
+awk_command <- 
+	paste(
+		"awk -F'\t' '{
+		printf \"%s\t\", $1;
+		for(i = 2; i <= NF; i += 4) {
+			printf \"%s\t\", $i;
+		}
+		print \"\"
+		}'",
+		methylation.file, 
+		"| sed 2d  >",
+		file.path(data.dir, "methylation-clean.txt"))
+
 # Execute the command
 system(awk_command)
 
