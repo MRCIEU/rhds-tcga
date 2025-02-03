3175cd101cc02a31a9c86894385085879a55060a
yousefi138 <paul.yousefi@bristol.ac.uk>
2025-02-03 17:13:13 +0000

    update report

--- analysis.r
+++ analysis.r
@@ -18,7 +18,7 @@ table(protein.names %in% colnames(data))
 ##TRUE 
 ## 109 
 
-## ----glms -------------------------------------------------------------
+## ----tissue -------------------------------------------------------------
 
 ## define glm formulae with pred.proteins as predictors of 'tumor.or.normal'
 ## tissue i.e. tumor.or.normal ~ pred.protein
@@ -51,26 +51,75 @@ fit.coefs <- {
 
 bonferroni <- -log10(0.05/length(fit))
 
-## ----manhattan -------------------------------------------------------------
-fit.coefs %>% 
+## ----tissue.figs -------------------------------------------------------------
+fit.coefs |>
 	ggplot(aes(x = pred.protein, y = -log10(p.value))) +
 		geom_point()+ 
 		theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
 		geom_text_repel(
-			data = fit.coefs[which(fit.coefs$scaled == 0 & 
-						(-log10(fit.coefs$p.value) > bonferroni)), ], 
+			data = fit.coefs[which((-log10(fit.coefs$p.value) > bonferroni)), ], 
 		    	aes(x=pred.protein,y=-log10(p.value),label=pred.protein))+ 
 		geom_hline(yintercept = bonferroni, 
 			linetype='dashed')
 
-## ----volcano -------------------------------------------------------------
-fit.coefs %>%
-	filter(scaled == 0) %>%
+fit.coefs |>
 		ggplot(aes(x = coef, y = -log10(p.value))) +
 		geom_point()+
 		geom_text_repel(
-			data = fit.coefs[which(fit.coefs$scaled == 0 & 
-						(-log10(fit.coefs$p.value) > bonferroni)), ], 
+			data = fit.coefs[which((-log10(fit.coefs$p.value) > bonferroni)), ], 
+		    	aes(x=coef,y=-log10(p.value),label=pred.protein))+ 
+		geom_hline(yintercept = bonferroni, 
+			linetype='dashed')
+
+
+## ----pfi -------------------------------------------------------------
+tumor.data <- subset(data, tumor ==1)
+
+## define glm formulae with pred.proteins as predictors of pfi
+## tissue i.e. pfi ~ pred.protein
+formulae <- sapply(protein.names, function(i){
+		reformulate(i, response = "pfi")
+		}, simplify = F)
+
+# run glms 
+fit <- sapply(formulae, function(i){
+	glm(i, data = tumor.data, family = binomial())
+	}, simplify = F)
+
+fit.summary <- sapply(fit ,function(i){
+	out <- summary(i)$coefficients
+	out[,"Estimate"] <- out[,"Estimate"]
+	out
+	}, simplify = F)
+
+fit.coefs <- sapply(fit.summary, function(i){
+	i[2, c("Estimate", "Pr(>|z|)")]
+	}, simplify = F) 
+fit.coefs <- {
+	x <- do.call(rbind, fit.coefs)
+	data.frame(
+		pred.protein = rownames(x),
+		coef = x[, "Estimate"],
+		p.value = x[, "Pr(>|z|)"]
+	)
+}
+
+## ----pfi.figs -------------------------------------------------------------
+fit.coefs |>
+	ggplot(aes(x = pred.protein, y = -log10(p.value))) +
+		geom_point()+ 
+		theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+		geom_text_repel(
+			data = fit.coefs[which((-log10(fit.coefs$p.value) > bonferroni)), ], 
+		    	aes(x=pred.protein,y=-log10(p.value),label=pred.protein))+ 
+		geom_hline(yintercept = bonferroni, 
+			linetype='dashed')
+
+fit.coefs |>
+		ggplot(aes(x = coef, y = -log10(p.value))) +
+		geom_point()+
+		geom_text_repel(
+			data = fit.coefs[which((-log10(fit.coefs$p.value) > bonferroni)), ], 
 		    	aes(x=coef,y=-log10(p.value),label=pred.protein))+ 
 		geom_hline(yintercept = bonferroni, 
 			linetype='dashed')
