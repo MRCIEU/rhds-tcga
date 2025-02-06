b1502f2fefcd1f7dca5ac66396d9143dacb32eb6
Gibran Hemani <explodecomputer@gmail.com>
2025-02-06 00:00:33 +0000

    removing md5

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -10,8 +10,8 @@ url="https://gdac.broadinstitute.org/runs/stddata__2016_01_28/data/HNSC/20160128
 while IFS=, read -r filename date time size
 do
     echo "Downloading $filename from $url"
-    curl -s -L $url/$filename -o $datadir/tcga/$filename
+    # curl -s -L $url/$filename -o $datadir/tcga/$filename
     curl -s -L $url/${filename}.md5 -o $datadir/tcga/${filename}.md5
     echo "Checking MD5"
-    md5sum -c $datadir/tcga/${filename}.md5
-done < data/files.csv
+    # md5sum -c $datadir/tcga/${filename}.md5
+done < files.csv
