6d1f83a4a6b2e7a598b033461cbb3d4e8757e509
Gibran Hemani <explodecomputer@gmail.com>
2025-02-06 00:33:49 +0000

    upd

--- download-pan-cancer-clinical.r
+++ download-pan-cancer-clinical.r
@@ -1,19 +1,22 @@
 library(readxl)
-stopifnot(dir.exists(paths$output.dir))
-
 # url & filename for clinical outcome data from the PanCancer Atlas project 
 # (https://gdc.cancer.gov/about-data/publications/pancanatlas)
 url <- "https://api.gdc.cancer.gov/data/1b5f413e-a8d1-4d10-92eb-7c4ae739ed81"
 filename <- "TCGA-CDR-SupplementalTableS1.xlsx"
 
 cat("download-pan-cancer-clinical.r", filename, "\n")
-if (!file.exists(file.path(paths$data.dir, filename)))
+if (!file.exists(filename))
+{
 	download.file(
 		url,
-		destfile=file.path(paths$data.dir, filename))
+		destfile=filename
+	)
+}
 
 # save a tab-seperated vesion 
-dat <- read_xlsx(file.path(paths$data.dir, filename), sheet=1)
-write.table(dat,
-	file=file.path(paths$data.dir, sub("xlsx$","txt",filename)), 
-    sep="\t",row.names=F,col.names=T)
+dat <- read_xlsx(filename, sheet=1)
+write.table(
+	dat,
+	file=sub("xlsx$", "txt", filename), 
+    sep="\t", row.names=F, col.names=T
+)
